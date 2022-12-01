44cd013e6928b8f67cf7e1f2acb29e8428bcc6ed
John Costa <johncosta027@gmail.com>
2022-12-01 10:39:44 +0000

    Day 1

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ export type DayFunc = (input: string) => [number, number];
 console.log("Advent Of Code - 2022");
 
 const day1input = await file('./inputs/day1.txt').text();
+console.time("Time Taken");
 const day1result = Day1(day1input);
 
 console.log("------------------Day 1--------------------")
 console.log("Part 1:", day1result[0])
 console.log("Part 2:", day1result[1])
+console.timeEnd("Time Taken");
 console.log("------------------------------------------")
