73ac6e0b7eed6e1f4d7c7c3aadab66d244e513aa
John Costa <johncosta027@gmail.com>
2022-12-04 12:50:11 +0000

    Day 4

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { file } from "bun";
 import { Day1 } from "./Day1/day1";
 import { Day2 } from "./Day2";
 import { Day3 } from "./Day3";
+import { Day4 } from "./Day4";
 
 export type DayFunc = (input: string) => [number, number];
 
@@ -27,7 +28,6 @@ console.log("Part 1:", day2result[0])
 console.log("Part 2:", day2result[1])
 console.timeEnd("Time Taken");
 console.log("------------------------------------------")
-*/
 
 const day3input = await file('./inputs/day3.txt').text();
 console.time("Time Taken");
@@ -38,3 +38,15 @@ console.log("Part 1:", day3result[0])
 console.log("Part 2:", day3result[1])
 console.timeEnd("Time Taken");
 console.log("------------------------------------------")
+*/
+
+const day4input = await file('./inputs/day4.txt').text();
+console.time("Time Taken");
+const day4result = Day4(day4input);
+
+console.log("------------------Day 3--------------------")
+console.log("Part 1:", day4result[0])
+console.log("Part 2:", day4result[1])
+console.timeEnd("Time Taken");
+console.log("------------------------------------------")
+
