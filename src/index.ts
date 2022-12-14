92e9fd2501364b0e8ea9e47f3106b6076c90786d
John Costa <johncosta027@gmail.com>
2022-12-14 14:50:41 +0000

    Day14: Really enjoyed this one, first try on Part2 second on part2!!!!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { file } from "bun";
-import { Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10, Day11, Day12, Day13 } from "./days";
+import {
+  Day1,
+  Day2,
+  Day3,
+  Day4,
+  Day5,
+  Day6,
+  Day7,
+  Day8,
+  Day9,
+  Day10,
+  Day11,
+  Day12,
+  Day13,
+  Day14,
+} from "./days";
 
 export type DayFunc = (input: string) => [any, any];
 
@@ -11,7 +26,11 @@ const RunDay = async (
   const dayInput = await file(`./inputs/${dayPath}.txt`).text();
   const [part1, part2] = dayFunc(dayInput);
 
-  console.log(`------------------Day ${dayPath.charAt(dayPath.length - 1)}--------------------`);
+  console.log(
+    `------------------Day ${dayPath.charAt(
+      dayPath.length - 1
+    )}--------------------`
+  );
   console.log("Part 1:", part1);
   console.log("Part 2:", part2);
   console.timeEnd("Time Taken");
@@ -33,5 +52,6 @@ console.time("total");
 // await RunDay(Day10, 'day10');
 // await RunDay(Day11, 'day11');
 // await RunDay(Day12, 'day12');
-await RunDay(Day13, 'day13');
+// await RunDay(Day13, 'day13');
+await RunDay(Day14, "day14");
 console.timeEnd("total");
